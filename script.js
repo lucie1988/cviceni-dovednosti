5f4aab0e58216fdb36da92c1fbad46796f550f97
Lucie Panagopulos <luciepanagopulos@gmail.com>
2024-03-08 13:46:01 +0100

    další úprava

--- script.js
+++ script.js
@@ -1,38 +1,54 @@
-let skillHtml = Number(prompt("uveď svoji úroveň dovedností v jazkyku HTML v rozsahu 0-100"))
-let skillCss = Number(prompt("uveď svoji úroveň dovedností v jazyku CSS v rozsahu 0-100"))
-let skillJs = Number(prompt("uveď svoji úroveň dovedností v jazyku JavaScript v rozsahu 0-100"))
+const HtmlLevel  = Number(prompt("Uveď svou úroveň dovedností v jazkyku HTML v rozsahu 0-100"));
+const CssLevel = Number(prompt("Uveď svou úroveň dovedností v jazyku CSS v rozsahu 0-100"));
+const JsLevel = Number(prompt("Uveď svou úroveň dovedností v jazyku JavaScript v rozsahu 0-100"));
 
-const updateSkill = (skillHtml,skillCss,skillJs) => {
+const updateSkill = (skill1) => {
 
     if (Html >= 0 && Html <= 100) {
-        updateSkill (skillHtml,skillId)
-        const progressBar = document.querySelector(`#${skillHtml} .skill__progress`);
-        progressBar.style.width = `${value}%`;
-    } else {
-        alert ("Neplatná hodnota pro dovednost Html!")
+        updateSkill = ("skill1", Html);//
         
-    }
-
-
-    if ((Css >= 0 && Css <= 100)) {
-        updateSkill (skillCss,skillId )
-        const progressBar = document.querySelector(`#${skillCss} .skill__progress`);
-        progressBar.style.width = `${value}%`;
     } else {
-        alert ("Neplatná hodnota pro dovednost CSS!")
+        alert ("Neplatná hodnota pro dovednost Html! Zadejte prosím číslo v rozsahu 0-100.");
+    }
+    {
+    updateSkill(Skill1, value)
+    const progressBar = document.querySelector(`#${skill1} .skill__progress`);
+    progressBar.style.width = `${value}%`;
+
+    }
+}
+
+const updateSkill = (skill2) => {
+
+    if (Css >= 0 && Css <= 100) {
+        updateSkill = ("skill2", Css);//
         
-    }
-
-
-    if ((Js >= 0 && Js <= 100)) {
-        updateSkill (skillJs,skillId)
-        const progressBar = document.querySelector(`#${skillJs} .skill__progress`);
-        progressBar.style.width = `${value}%`;
     } else {
-        alert ("Neplatná hodnota pro dovednost JavaScript!")
+        alert ("Neplatná hodnota pro dovednost Css! Zadejte prosím číslo v rozsahu 0-100.");
+    }
+    {
+    updateSkill(skill2, value)
+    const progressBar = document.querySelector(`#${skill2} .skill__progress`);
+    progressBar.style.width = `${value}%`;
+
+    }
+}
+
+const updateSkill = (skill3) => {
+
+    if (Js >= 0 && Js <= 100) {
+        updateSkill = ("skill3", Js);//
+        
+    } else {
+        alert ("Neplatná hodnota pro dovednost Js! Zadejte prosím číslo v rozsahu 0-100.");
+    }
+    {
+    updateSkill(skill3, value)
+    const progressBar = document.querySelector(`#${skill3} .skill__progress`);
+    progressBar.style.width = `${value}%`;
         
     }
 }
-console.log (updateSkill(skillHtml,skill1)) 
-console.log (updateSkill(skillCss,skill2)) 
-console.log (updateSkill(skillJs,skill3))
+updateSkill(HtmlLevel, "skill1");
+updateSkill(CssLevel, "skill2");
+updateSkill(JsLevel, "skill3");
