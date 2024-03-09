536be1f9fbfa153b2790a9a5bf694721f32fe9d6
Lucie Panagopulos <luciepanagopulos@gmail.com>
2024-03-09 01:23:00 +0100

    final

--- script.js
+++ script.js
@@ -1,54 +1,29 @@
-const HtmlLevel  = Number(prompt("Uveď svou úroveň dovedností v jazkyku HTML v rozsahu 0-100"));
-const CssLevel = Number(prompt("Uveď svou úroveň dovedností v jazyku CSS v rozsahu 0-100"));
-const JsLevel = Number(prompt("Uveď svou úroveň dovedností v jazyku JavaScript v rozsahu 0-100"));
+const skillHtml = Number(prompt("Uveď svou úroveň dovedností v jazyku HTML v rozsahu 0-100"))
+const skillCss = Number(prompt("Uveď svou úroveň dovedností v jazyku CSS v rozsahu 0-100"))
+const skillJs = Number(prompt("Uveď svou úroveň dovedností v jazyku JavaScript v rozsahu 0-100"))
 
-const updateSkill = (skill1) => {
-
-    if (Html >= 0 && Html <= 100) {
-        updateSkill = ("skill1", Html);//
-        
-    } else {
-        alert ("Neplatná hodnota pro dovednost Html! Zadejte prosím číslo v rozsahu 0-100.");
-    }
-    {
-    updateSkill(Skill1, value)
-    const progressBar = document.querySelector(`#${skill1} .skill__progress`);
-    progressBar.style.width = `${value}%`;
-
-    }
+function updateSkill(skill1, level) {
+    let skillId = document.getElementById(skill1)
+    let progress = skillId.querySelector(".skill__progress")
+    progress.style.width = level + "%"
+    let value = skillId.querySelector(".skill__value")
+    value.textContent = level + " / 100"
+}
+function updateSkill(skill2, level) {
+    let skillId = document.getElementById(skill2)
+    let progress = skillId.querySelector(".skill__progress")
+    progress.style.width = level + "%"
+    let value = skillId.querySelector(".skill__value")
+    value.textContent = level + " / 100"
+}
+function updateSkill(skill3, level) {
+    let skillId = document.getElementById(skill3)
+    let progress = skillId.querySelector(".skill__progress")
+    progress.style.width = level + "%"
+    let value = skillId.querySelector(".skill__value")
+    value.textContent = level + " / 100"
 }
 
-const updateSkill = (skill2) => {
-
-    if (Css >= 0 && Css <= 100) {
-        updateSkill = ("skill2", Css);//
-        
-    } else {
-        alert ("Neplatná hodnota pro dovednost Css! Zadejte prosím číslo v rozsahu 0-100.");
-    }
-    {
-    updateSkill(skill2, value)
-    const progressBar = document.querySelector(`#${skill2} .skill__progress`);
-    progressBar.style.width = `${value}%`;
-
-    }
-}
-
-const updateSkill = (skill3) => {
-
-    if (Js >= 0 && Js <= 100) {
-        updateSkill = ("skill3", Js);//
-        
-    } else {
-        alert ("Neplatná hodnota pro dovednost Js! Zadejte prosím číslo v rozsahu 0-100.");
-    }
-    {
-    updateSkill(skill3, value)
-    const progressBar = document.querySelector(`#${skill3} .skill__progress`);
-    progressBar.style.width = `${value}%`;
-        
-    }
-}
-updateSkill(HtmlLevel, "skill1");
-updateSkill(CssLevel, "skill2");
-updateSkill(JsLevel, "skill3");
+updateSkill("skill1", skillHtml)
+updateSkill("skill2", skillCss)
+updateSkill("skill3", skillJs)
