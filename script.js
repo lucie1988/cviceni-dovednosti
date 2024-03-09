a1e3000aaa6814e0a11ffe5f4ccd51c98fc730af
Lucie Panagopulos <luciepanagopulos@gmail.com>
2024-03-09 23:52:05 +0100

    přidání alert

--- script.js
+++ script.js
@@ -9,6 +9,14 @@ function updateSkill(skill1, level) {
     let value = skillId.querySelector(".skill__value")
     value.textContent = level + " / 100"
 }
+    if (skillHtml >=0 && skillHtml <=100) { 
+        updateSkill("skill1", skillHtml)
+        } 
+        
+        else {
+        alert("Neplatná hodnota pro dovednost HTML! Zadejte prosím číslo v rozsahu 0-100.")
+        }
+
 function updateSkill(skill2, level) {
     let skillId = document.getElementById(skill2)
     let progress = skillId.querySelector(".skill__progress")
@@ -16,6 +24,14 @@ function updateSkill(skill2, level) {
     let value = skillId.querySelector(".skill__value")
     value.textContent = level + " / 100"
 }
+    if (skillCss >=0 && skillCss <=100) { 
+        updateSkill("skill2", skillCss)
+        } 
+        
+        else {
+        alert ("Neplatná hodnota pro dovednost CSS! Zadejte prosím číslo v rozsahu 0-100.")
+        }
+
 function updateSkill(skill3, level) {
     let skillId = document.getElementById(skill3)
     let progress = skillId.querySelector(".skill__progress")
@@ -23,6 +39,14 @@ function updateSkill(skill3, level) {
     let value = skillId.querySelector(".skill__value")
     value.textContent = level + " / 100"
 }
+    if (skillJs >=0 && skillJs <=100) { 
+        updateSkill("skill3", skillJs)
+        } 
+        
+        else {
+        alert ("Neplatná hodnota pro dovednost JavaScript! Zadejte prosím číslo v rozsahu 0-100.")
+        }
+
 
 updateSkill("skill1", skillHtml)
 updateSkill("skill2", skillCss)
